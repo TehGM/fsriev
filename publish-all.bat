3e20b85fefbbc904513ca29b1be5cd7c48861e34
TehGM <wojop95@gmail.com>
2021-03-22 11:50:21 +0100

    Create publish profiles and script

---
+++ publish-all.bat
@@ -0,0 +1,35 @@
+@echo off
+
+set outputdir=fsriev\bin\Output\
+set publishdir=fsriev\bin\Release\net5.0\publish\
+if not exist %outputdir% mkdir %outputdir%
+
+set name="Portable (Framework Dependent)"
+echo Publishing: %name%
+dotnet publish -p:PublishProfile=%name%
+tar -a -c -f %outputdir%fsriev.Portable.zip -C %publishdir%portable *
+
+set name="Win x86"
+echo Publishing: %name%
+dotnet publish -p:PublishProfile=%name%
+tar -a -c -f %outputdir%fsriev.Win-x86.zip -C %publishdir%win-x86 *
+
+set name="Win x64"
+echo Publishing: %name%
+dotnet publish -p:PublishProfile=%name%
+tar -a -c -f %outputdir%fsriev.Win-x64.zip -C %publishdir%win-x64 *
+
+set name="Win ARM"
+echo Publishing: %name%
+dotnet publish -p:PublishProfile=%name%
+tar -a -c -f %outputdir%fsriev.Win-ARM.zip -C %publishdir%win-arm *
+
+set name="Linux x64"
+echo Publishing: %name%
+dotnet publish -p:PublishProfile=%name%
+tar -a -c -f %outputdir%fsriev.Linux-x64.zip -C %publishdir%linux-x64 *
+
+set name="Linux ARM"
+echo Publishing: %name%
+dotnet publish -p:PublishProfile=%name%
+tar -a -c -f %outputdir%fsriev.Linux-ARM.zip -C %publishdir%linux-arm *

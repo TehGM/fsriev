403cd45efdba2fd1f9a4ae2385abc74eebb148a6
TehGM <wojop95@gmail.com>
2021-06-15 20:14:30 +0200

    Added default log config as code

--- fsriev/Logging/LoggingInitializationExtensions.cs
+++ fsriev/Logging/LoggingInitializationExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
@@ -6,27 +8,55 @@ namespace TehGM.Fsriev.Logging
 {
     public static class LoggingInitializationExtensions
     {
+        private const string _sectionName = "Logging";
+
         public static IHostBuilder ConfigureSerilog(this IHostBuilder builder)
             => builder.UseSerilog(ConfigureSerilog, true);
 
         public static void ConfigureSerilog(HostBuilderContext context, LoggerConfiguration config)
         {
-            config.ReadFrom.Configuration(context.Configuration, "Logging")
-                .Enrich.FromLogContext();
+            if (context.Configuration.GetSection(_sectionName).Exists())
+                config.ReadFrom.Configuration(context.Configuration, _sectionName);
+            else
+                config
+                    .AddSharedConfiguration()
+                    .AddFileDefaults("log");
+        }
+
+        private static LoggerConfiguration AddSharedConfiguration(this LoggerConfiguration config)
+        {
+            return config
+                .Enrich.FromLogContext()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", Serilog.Events.LogEventLevel.Warning)
+                .WriteTo.Console();
+        }
+
+        private static LoggerConfiguration AddFileDefaults(this LoggerConfiguration config, string logFileName)
+        {
+            if (config == null)
+                throw new ArgumentNullException(nameof(config));
+            if (string.IsNullOrWhiteSpace(logFileName))
+                throw new ArgumentNullException(nameof(logFileName));
+            // get file path
+            string dir = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            string path = Path.Combine(dir, "TehGM/fsriev/logs/", $"{logFileName}-.log");
+            // build config
+            return config.WriteTo.Async(sink =>
+                sink.File(path,
+                    fileSizeLimitBytes: 1048576,        // 1MB
+                    rollOnFileSizeLimit: true,
+                    retainedFileCountLimit: 30,
+                    rollingInterval: RollingInterval.Day));
         }
 
         public static void EnableUnhandledExceptionLogging()
         {
             // add default logger for errors that happen before host runs
-            string dir = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
             Log.Logger = new LoggerConfiguration()
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .WriteTo.File($"{dir}/TehGM/fsriev/logs/unhandled-.log",
-                fileSizeLimitBytes: 1048576,        // 1MB
-                rollOnFileSizeLimit: true,
-                retainedFileCountLimit: 30,
-                rollingInterval: RollingInterval.Day)
+                .AddSharedConfiguration()
+                .AddFileDefaults("unhandled")
                 .CreateLogger();
             // capture unhandled exceptions
             AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
